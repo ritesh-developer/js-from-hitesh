c9bf3c2683082ae2bbf2bbcbb5028efc19daee63
ritesh <riteshraj.developer@gmail.com>
2024-10-30 12:01:31 +0530

    😄❤

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict"; /*  ye JS ko new version jai sa treat kare ga 
+                treat all javaScript code as a new version ES6 likely to be precised
+                read more about this on chat gpt please (ES6) so called new version of javaScript */
+
+  // alert (3+3)      // We are useing node js not Browser the engain of JavaScript is hidden in the broweser        
+
+  console.log ("Ritesh")  // code readablity is the key So, plaese write a readable code for the future proof 
+  console.log (99+1)  // code readability should be high ,
+
+ console.log ( 99.666 +944944) 
+ let fruits = ["apple",  "banana", "cherry"];
+ 
+
+ console.log (fruits)
