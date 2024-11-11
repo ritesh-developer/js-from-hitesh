1781463c12cab32a01ab571f186150dc770c24aa
ritesh <riteshraj.developer@gmail.com>
2024-11-11 11:47:41 +0530

    DataType in JavaScript

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -5,11 +5,11 @@
   // alert (3+3)      // We are useing node js not Browser the engain of JavaScript is hidden in the broweser        
 
   console.log ("Ritesh")  // code readablity is the key So, plaese write a readable code for the future proof 
-  console.log (99+1)  // code readability should be high ,
+  console.log (99+1)  // code readability should be high ,j
 
  console.log ( 99.666 +944944) 
  let fruits = ["apple",  "banana", "cherry"];
- 
+
  
 >
 
